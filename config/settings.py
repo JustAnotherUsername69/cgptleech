d231a5ae34a01ef5ff80602d34f128bf576f45d7
Akshay Bangad <68388096+JustAnotherUsername69@users.noreply.github.com>
2025-09-16 17:30:16 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,11 @@
-# Replace with your actual Telegram Bot API key
-TELEGRAM_API_KEY = "YOUR_BOT_API_KEY"
+# config/settings.py
+
+# Replace with your actual API ID and API HASH from Telegram's Developer Portal
+API_ID = "1301652"
+API_HASH = "407daccd35057d3b76e9533f4dbda814"
+
+# Replace with the session string generated via Pyrogram
+SESSION_STRING = "BQAT3JQAoQWG5zOFKKKHLROZmXCrZd2gnODeHetyDQCkL636ajqKujr5DwkmjPxQgVM-YIQG3kzdWcCzAy5Ye1Mz8zFQGqiyHRHM4bFNh2UhUZCu9Dcc4fdTjgJEMs83StL6xraa9ZcaiFBBu1xQaCsCC_WXy9ZawkNGi6sX50eKXrGaMSvbLGCAvf2RwlOPLlsd_c7Wj6L2rxxuVXtA_XyPuf2gtNv8SpUaTGDfyp4C6lB16NLlJGDZEqM4jR5V5imKdBcbHPIiNu7Mk3SS2dhEXt2SFHLMdhVUJFAo4lESSS_hTsOLYdhytwKfC4isHHlGIzk0qlWmD5ovS20lnlaNl9rFAwAAAAAoESFRAA"
+
+# Bot API Token from BotFather
+TELEGRAM_API_KEY = "8452403186:AAG5gb3pE2bI2G29tn3vdIJ-A5XTtyFXuH4"
